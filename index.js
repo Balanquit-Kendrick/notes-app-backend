2044ebc9f6af689ae9bacdd87b2cd9321a664702
unknown <zxckendrick123@gmail.com>
2024-07-10 15:34:41 +0800

    search note

--- index.js
+++ index.js
@@ -292,6 +292,41 @@ app.put('/update-note-pinned/:noteId', authenticateToken, async (req,res) => {
     });
   }
 });
+
+// Search Note
+app.get('/search-note/', authenticateToken, async (req,res) => {
+  const { user } = req.user;
+  const { query } = req.query;
+
+  if (!query) {
+    return res
+     .status(400)
+     .json({ error: true, message: "Search query is required" });
+  }
+
+  try {
+    const matchingNotes = await Note.find({
+      userId: user._id,
+      $or: [
+        { title: {$regex: new RegExp(query, "i")}},
+        { content: {$regex: new RegExp(query, "i")}},
+      ],
+    });
+
+    return res.json({
+      error: false,
+      notes: matchingNotes,
+      message: "Notes matching the search query retrievd successfully",
+    });
+
+
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal Server Error",
+    });
+  }
+});
 app.listen(8000);
 
 
